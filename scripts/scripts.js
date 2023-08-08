7cfe75ae20e426f4d5004fa4bbe1d6e3b0d842cc
wk-bootdee <keawbootdeew@student.douglascollege.ca>
2023-08-07 23:28:18 -0700

    init

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+
+function displayText() {
+    const textDisplay = document.getElementById('displayText');
+    // Toggle the display property of the text element
+    textDisplay.style.display = (textDisplay.style.display === 'none') ? 'block' : 'none';
+}
